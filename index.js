6abea34f051ba0a7258d36200c4a12496009342e
rania248 <raniakthiri25@gmail.com>
2021-09-10 13:01:28 +0100

    mongoose

--- index.js
+++ index.js
@@ -17,19 +17,15 @@ mongoose
     console.log(err);
   });
 
-
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
 
-
 app.post("/CreateAndSave", async (req, res) => {
-   
   try {
-    const {name, age, favoriteFoods } = req.body;
-    const contact = new Contact({ name,age , favoriteFoods});
-		await contact.save();
+    const { name, age, favoriteFoods } = req.body;
+    const contact = new Contact({ name, age, favoriteFoods });
+    await contact.save();
     res.status(200).send({ msg: "contact added", Contact });
   } catch (error) {
     console.log(error);
@@ -37,92 +33,80 @@ app.post("/CreateAndSave", async (req, res) => {
   }
 });
 
-app.post ("/createMany", async (req,res,done)=> {
-try {
-    const {name, age, favoriteFoods } = req.body;
+app.post("/createMany", async (req, res, done) => {
+  try {
+    const { name, age, favoriteFoods } = req.body;
 
-await Contact.create(arrayOfContact, (err, data) => {
-      if(err) {
-         done(err); 
+    await Contact.create(arrayOfContact, (err, data) => {
+      if (err) {
+        done(err);
       }
-    done(null, data);
-    }) 
-
-}
-catch(error) {
+      done(null, data);
+    });
+  } catch (error) {
     console.log(error);
     res.status(500).json({ status: false, message: error });
   }
-})
+});
 
-app.get ("/findByID", async(req,res) => {
-    const contact = await Contact.findById("613a1bd651be70a9047bb0f1")
-    console.log(contact)
-})
+app.get("/findByID", async (req, res) => {
+  const contact = await Contact.findById("613a1bd651be70a9047bb0f1");
+  console.log(contact);
+});
 
+app.get("/findByName", async (req, res, done) => {
+  await findContactByName(name, done);
+  Contact.find({ name: name }, (err, data) => {
+    if (err) return done(err);
+    return done(null, data);
+  });
+});
 
-app.get ("/findByName", async(req,res,done) => {
-
-await findContactByName (name, done) 
-    Contact.find({"name":name},(err,data)=>{
-    if(err) return done(err)
-      return done(null,data)
-    
-    })
-
-})
-
-app.get ("/findOneByFood", async(req,res,done)=>{
-
-const contact = await    Contact.find({favoriteFoods:food},(err,data)=>{
+app.get("/findOneByFood", async (req, res, done) => {
+  const contact = await Contact.find({ favoriteFoods: food }, (err, data) => {
     //  console.log(food);
     //  console.log(data);
-      if(err) return done(err)
-       done(null,data)
-    }); 
-console.log(contact)
+    if (err) return done(err);
+    done(null, data);
+  });
+  console.log(contact);
+});
 
-})
+app.put("/findOneAndUpdate", async (req, res) => {
+  await Contact.findByIdAndUpdate("612df3bbeb561e1e783cf727", {
+    name: "jaouher",
+  });
+});
 
-  
-   
-app.put("/findOneAndUpdate",async (req,res)=>{
-
-    await Contact.findByIdAndUpdate("612df3bbeb561e1e783cf727", {name : "jaouher"})
-})
-
-
-app.put("/findEditThenSave", async (req,res)=>{
-
-const contact = await Contact.findById(contactId, function(err, data) {
+app.put("/findEditThenSave", async (req, res) => {
+  const contact = await Contact.findById(contactId, function (err, data) {
     data.favoriteFoods.push(foodToAdd).save();
+  });
+});
 
-})
-})
+app.delete("/emoveById,", async (req, res, done) => {
+  const contact = await Contact.findByIdAndRemove(contactId, (err, data) =>
+    err ? done(err) : done(null, data)
+  );
+});
 
+app.delete("/removeMany", async (req, res, done) => {
+  Contact.deleteMany({ name: nameToRemove }, function (err, data) {
+    if (err) {
+      done(err);
+    } else {
+      done(null, data);
+    }
+  });
+});
 
-app.delete("/emoveById,", async (req,res,done)=>{
-
-    const contact = await Contact.findByIdAndRemove(contactId, (err, data) => err ? done(err) : done(null, data));
-
-})
-
-app.delete("/removeMany",async(req,res,done)=>{
-    Contact.deleteMany({name: nameToRemove}, function(err, data) {
-        if (err) {
-          done(err);
-        } else {
-          done(null, data);
-        }
-      });
-
-
-})
-
-app.get("/queryCain", async(req,res,done)=>{
-    Contact.find({favoriteFoods:foodToSearch}).sort({name : "desc"}).limit(2).select("-age").exec((err, data) => {
-        if(err)
-          done(err);
-       done(null, data);
-     })
-})
+app.get("/queryCain", async (req, res, done) => {
+  Contact.find({ favoriteFoods: foodToSearch })
+    .sort({ name: "desc" })
+    .limit(2)
+    .select("-age")
+    .exec((err, data) => {
+      if (err) done(err);
+      done(null, data);
+    });
+});
